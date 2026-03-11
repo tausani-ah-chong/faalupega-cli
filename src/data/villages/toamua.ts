5bea6fb28c0eda66c6db907bde8118f47433c113
Tausani Ah Chong <58315812+tausani-ah-chong@users.noreply.github.com>
2026-03-11 13:21:11 +1300

    Fix typo in village descriptions

--- src/data/villages/toamua.ts
+++ src/data/villages/toamua.ts
@@ -8,7 +8,7 @@ export const toamua: Village = {
   tulou: [
     "Tulouna lau tofa Ale ma Ina\u02BBilau na oo i lo outou aiga, o Tupalaimuli ma Tauaiupolu, ma le Aoaniu ma le Momoti",
     "Tulouna oe le matua o lau tofa Puipui",
-    "Tulouna oe le itu lua o Satunumafono o Palusalue ma Aunuagaolo ma Taoa",
+    "Tulouna oe le itu lua o Satunumafono o Palusalue ma Aumuagaolo ma Taoa",
   ],
 
   saotamaitai: [],
